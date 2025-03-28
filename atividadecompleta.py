f119ac3baebd40069546f4f7f63d61c39cf4e9ff
Alinny <149887638+nogalinny@users.noreply.github.com>
2025-03-28 10:28:08 -0300

    Update atividadecompleta.py
    Adicionando alteração no gráfico de pizza.

--- atividadecompleta.py
+++ atividadecompleta.py
@@ -54,6 +54,36 @@ plt.ylabel('Qtd_Vendidos_Cod')
 plt.xticks(rotation=0)
 plt.show()
 
+genero_counts = df.groupby('Gênero')['Qtd_Vendidos_Cod'].sum()
+
+# Definir cores específicas para cada categoria de gênero
+cores = {
+    'Bebês': '#ff9999',         # Rosa claro
+    'Feminino': '#ff66b2',      # Rosa forte
+    'Masculino': '#66b3ff',     # Azul
+    'Meninas': '#ffcc99',       # Laranja claro
+    'Meninos': '#3399ff',       # Azul forte
+    'Sem gênero': '#c2c2c2',    # Cinza
+    'Sem gênero infantil': '#8c8c8c',  # Cinza escuro
+    'Unissex': '#99ff99',        # Verde claro
+    'roupa para gordinha pluss P ao 52': '#9966cc' # Roxo
+}
+
+# Aplicar as cores conforme os gêneros encontrados no dataset
+cores_usadas = [cores[categoria] for categoria in genero_counts.index]
+
+# Criar o gráfico de pizza
+plt.figure(figsize=(8, 6))
+wedges, texts, autotexts = plt.pie(
+    genero_counts, labels=None, autopct='%.1f%%', startangle=90, colors=cores_usadas
+)
+
+# Adicionar a legenda ao lado
+plt.legend(wedges, genero_counts.index, title="Gênero", loc="center left", bbox_to_anchor=(1, 0.5))
+
+plt.title('Quantidade de Produtos Vendidos por Gênero')
+plt.show()
+
 # Gráfico de densidade de salários
 plt.figure(figsize=(10, 6))  # Define o tamanho da figura
 sns.kdeplot(df['Preço_MinMax'], fill=True, color='#863e9c')  # Cria um gráfico de densidade (KDE) preenchido
